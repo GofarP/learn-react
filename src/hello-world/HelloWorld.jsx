1676acf11004308b94036fa96ab0d15da8e861a5
Gofar Putra <gofarperdana83@gmail.com>
2025-07-05 11:29:21 +0700

    remove

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,21 @@
+export default function HelloWorld() {
+    return (
+        <div>
+        <HeaderHelloWorld/>
+        <ParagraphHelloWorld/>
+        </div>
+    )
+}
+
+function HeaderHelloWorld() {
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+function ParagraphHelloWorld() {
+    return (
+        <p>Selamat Belajar React</p>
+
+    )
+}
