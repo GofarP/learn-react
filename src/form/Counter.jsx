0ca1dac22f12b2f2e0f2a93ce2660340a662ee82
Gofar Putra <gofarperdana83@gmail.com>
2025-07-22 05:55:25 +0700

    Use state

---
+++ src/form/Counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Counter(){
+    let counter=0;
+    const [newCounter, setnewCounter]=useState(0)
+    return(
+        <div>
+            <button onClick={()=>{
+                setnewCounter(newCounter => newCounter+1)
+            }}>Increment</button>
+
+            <h1>Counter: {newCounter    } </h1>
+        </div>
+    )
+}
